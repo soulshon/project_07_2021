98f2ae420a01903365c470d08712a053c42aee4d
Platon <soulshon@mail.ru>
2021-07-20 17:05:57 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,62 @@
+//02 Основы JavaScript c 18/07/2021
+//  console.log(12);
+"use strict";
+// a = 10; c активированным use strick не будет работать
+// console.log(a);
+
+// let number = 4.6;
+// console.log(number);
+//Вариант объявления строковых данных
+// const persone = "Alex";
+// const persone1 = `Alex`;
+// const persone2 = 'Alex';
+
+// let und;
+// console.log(und); - будет undefined
+
+// const obj = {
+// name: 'Ваня',
+// age: 25,
+// isMarried: false
+// };
+
+// // console.log(obj.name);
+// // console.log(obj["name"]]); - 2-й вариант обращения к значению ключа;
+
+// //Массив является частным сулчаем объекта
+// let arr = ['plum.png', 'orange.jpg', 'appel.bmp'];
+// console.log(arr[1]); 
+// alert('Hello');
+// const result = confirm("Are you here?");
+//по умолчани. от пользовтеля поступают значеия в виде строки (если поставить НАПРИМЕР +перед prompt - то можено будет получить числовое знаечни "number");
+// const answer = +prompt("Вам есть 18?",'18');
+// console.log(typeof(answer));
+
+//пример того как ответы от пользовтеля могут сразу  формироваться в массив
+// const answers = [];
+// answers[0]= prompt('Как ваше имя?', '');
+// answers[1]= prompt('Какая у вас фамилия?', '');
+// answers[2]= prompt('Сколько вам лет?', '');
+
+// document.write (answers);
+//Интреполяция 19.07.2021
+// const catregory = 'toys';
+// console.log('https://someurl.com/'+catregory); //пример контикинации (склеивания) строки; 
+// console.log(`https://someurl. com/${catregory}/5`);//пример интерполяции строки; 
+
+
+// //Пример интреполяци 
+// let user = 'Ivan';
+// alert(user);
+// Операторы
+//+
+//console.log (4+ +"5"); //+перед "5" называется унарным (использует 1 элемент для своей работы ) и присваивает строке числовое значение.
+//Инкремент и дикремент
+// let incr = 10,
+//     decr = 10;
+// // incr++;
+// // decr--;
+// console.log(++incr);
+// console.log(--decr);
+//  console. log(11%4);
+console.log(2*2+2!=='8');
